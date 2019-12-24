f864c7b6a60798d33aab6820c5ea78eb3aaa0cc3
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2019-12-24 14:42:50 +0100

    divide KsiazkaAdresowa class into smaller classes

---
+++ PlikZUzytkownikami.h
@@ -0,0 +1,25 @@
+#ifndef PLIKZUZYTKOWNIKAMI_H
+#define PLIKZUZYTKOWNIKAMI_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "MetodyPomocnicze.h"
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class PlikZUzytkownikami{
+
+    string nazwaPlikuZUzytkownikami;
+    fstream plikTekstowy;
+
+    bool czyPlikJestPusty();
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+
+public:
+    PlikZUzytkownikami();
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+};
+
+#endif
